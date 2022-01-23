edb48db540252ae2f22500dbc0315b68c082d010
Olt Latifi <oltlatifi2003@gmail.com>
2022-01-23 19:41:00 +0100

    making ready for deployment

--- literate_api/settings.py
+++ literate_api/settings.py
@@ -7,7 +7,9 @@ from django.conf import settings
 env = environ.Env()
 environ.Env.read_env()
 
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -19,9 +21,9 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost:8000", "oltlatifi.pythonanywhere.com"]
 
 
 # Application definition
